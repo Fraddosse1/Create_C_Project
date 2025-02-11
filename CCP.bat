074369b7bf8e1cf38618bfdb30ff153c9eca39b8
Fraddosse_Laptop <fraddosse@hotmail.com>
2025-02-11 15:56:14 +0100

    Added project name configuration

--- CCP.bat
+++ CCP.bat
@@ -4,19 +4,27 @@
 @echo off
 
 echo =======================================
-echo           _____ _____ _____  
+echo           _____ _____ _____
 echo          ^/ ____^/ ____^|  __ ^\ 
-echo         ^| ^|   ^| ^|    ^| ^|__^) ^|
+echo         ^| ^|   ^| ^|    ^| ^|__^)^|
 echo         ^| ^|   ^| ^|    ^|  ___^/ 
 echo         ^| ^|___^| ^|____^| ^|     
-echo          ^\_____^\_____^|_^|                                   
-echo =======================================                  
+echo          ^\_____^\_____^|_^|                          
+echo =======================================
 echo.
+
+:: Input the name of the project
+:NameProject
+echo.
+set /p Name=^> Enter the name of the project : 
+set /p Verification=^> Are you sure ? ^(Y/N^) : 
+
+if /i "%Verification%"=="n" goto NameProject
 
 :: Input type of project (c/cpp)
 :InputType
 echo.
-set /p type=Enter the type of project (c/cpp): 
+set /p type=^> Enter the type of project (c/cpp) : 
 
 if /i "%type%" NEQ "c" if /i "%type%" NEQ "cpp" (
     echo.
@@ -27,7 +35,7 @@ if /i "%type%" NEQ "c" if /i "%type%" NEQ "cpp" (
 :: Input the PATH of the project
 :InputPath
 echo.
-set /p path=Enter the path of the project :
+set /p path=^> Enter the path of the project : 
 
 if "%path%" == "" (
     echo.
@@ -54,12 +62,12 @@ cd %path%
 :: Création des répertoires du projet
 echo.
 echo Creation des dossiers...
-mkdir Project_Name^
-    Project_Name\conf^
-    Project_Name\bin^
-    Project_Name\Project\src^
-    Project_Name\Project\inc^
-    Project_Name\Project\res
+mkdir %Name%^
+    %Name%\conf^
+    %Name%\bin^
+    %Name%\Project\src^
+    %Name%\Project\inc^
+    %Name%\Project\res
 
 echo Dossiers cree avec succes.
 
@@ -71,23 +79,23 @@ if /i "%type%"=="c" (
     echo Creation des fichiers .c ...
 
     :: Création des fichiers .c
-    echo. > Project_Name\Project\src\main.c
-    echo. > Project_Name\Project\src\Functions.c    
+    echo. > %Name%\Project\src\main.c
+    echo. > %Name%\Project\src\Functions.c    
 
     :: Remplissage par défaut main.c
-    echo #include ^<stdio.h^> > Project_Name\Project\src\main.c
-    echo. >> Project_Name\Project\src\main.c
-    echo int main^(^) { >> Project_Name\Project\src\main.c
-    echo. >> Project_Name\Project\src\main.c
-    echo printf^("Hello world"^); >> Project_Name\Project\src\main.c
-    echo. >> Project_Name\Project\src\main.c
-    echo } >> Project_Name\Project\src\main.c
+    echo #include ^<stdio.h^> > %Name%\Project\src\main.c
+    echo. >> %Name%\Project\src\main.c
+    echo int main^(^) { >> %Name%\Project\src\main.c
+    echo. >> %Name%\Project\src\main.c
+    echo printf^("Hello world"^); >> %Name%\Project\src\main.c
+    echo. >> %Name%\Project\src\main.c
+    echo } >> %Name%\Project\src\main.c
 
     :: Remplissage par défaut Functions.c
-    echo #include ^<stdio.h^> > Project_Name\Project\src\Functions.c
-    echo #include "../inc/Functions.h" >> Project_Name\Project\src\Functions.c
-    echo. >> Project_Name\Project\src\Functions.c
-    echo // Functions content >> Project_Name\Project\src\Functions.c
+    echo #include ^<stdio.h^> > %Name%\Project\src\Functions.c
+    echo #include "../inc/Functions.h" >> %Name%\Project\src\Functions.c
+    echo. >> %Name%\Project\src\Functions.c
+    echo // Functions content >> %Name%\Project\src\Functions.c
 
     :: Message de succès
     echo Fichiers .c cree avec succes.
@@ -97,15 +105,15 @@ if /i "%type%"=="c" (
     echo Creation des fichiers .h ...
         
     :: Création des fichiers .h
-    echo. > Project_Name\Project\inc\Functions.h
+    echo. > %Name%\Project\inc\Functions.h
 
     :: Remplissage par défaut Functions.h
-    echo #ifndef __FUNCTIONS__ > Project_Name\Project\inc\Functions.h
-    echo #define __FUNCTIONS__ >> Project_Name\Project\inc\Functions.h
-    echo. >> Project_Name\Project\inc\Functions.h
-    echo // Header content >> Project_Name\Project\inc\Functions.h
-    echo. >> Project_Name\Project\inc\Functions.h
-    echo #endif >> Project_Name\Project\inc\Functions.h
+    echo #ifndef __FUNCTIONS__ > %Name%\Project\inc\Functions.h
+    echo #define __FUNCTIONS__ >> %Name%\Project\inc\Functions.h
+    echo. >> %Name%\Project\inc\Functions.h
+    echo // Header content >> %Name%\Project\inc\Functions.h
+    echo. >> %Name%\Project\inc\Functions.h
+    echo #endif >> %Name%\Project\inc\Functions.h
 
     echo Fichiers .h cree avec succes.
 ) else (
@@ -114,23 +122,23 @@ if /i "%type%"=="c" (
     echo Creation des fichiers .cpp ...
 
     :: Création des fichiers .cpp
-    echo. > Project_Name\Project\src\main.cpp
-    echo. > Project_Name\Project\src\Functions.cpp
+    echo. > %Name%\Project\src\main.cpp
+    echo. > %Name%\Project\src\Functions.cpp
 
     :: Remplissage par défaut main.cpp
-    echo #include ^<iostream^> > Project_Name\Project\src\main.cpp
-    echo. >> Project_Name\Project\src\main.cpp
-    echo int main^(^) { >> Project_Name\Project\src\main.cpp
-    echo. >> Project_Name\Project\src\main.cpp
-    echo std::cout ^<^< "Hello world" ^<^< std::endl; >> Project_Name\Project\src\main.cpp
-    echo. >> Project_Name\Project\src\main.cpp
-    echo } >> Project_Name\Project\src\main.cpp
+    echo #include ^<iostream^> > %Name%\Project\src\main.cpp
+    echo. >> %Name%\Project\src\main.cpp
+    echo int main^(^) { >> %Name%\Project\src\main.cpp
+    echo. >> %Name%\Project\src\main.cpp
+    echo std::cout ^<^< "Hello world" ^<^< std::endl; >> %Name%\Project\src\main.cpp
+    echo. >> %Name%\Project\src\main.cpp
+    echo } >> %Name%\Project\src\main.cpp
 
     :: Remplissage par défaut Functions.cpp
-    echo #include ^<iostream^> > Project_Name\Project\src\Functions.cpp
-    echo #include "../inc/Functions.hpp" >> Project_Name\Project\src\Functions.cpp
-    echo. >> Project_Name\Project\src\Functions.cpp
-    echo // Functions content >> Project_Name\Project\src\Functions.cpp
+    echo #include ^<iostream^> > %Name%\Project\src\Functions.cpp
+    echo #include "../inc/Functions.hpp" >> %Name%\Project\src\Functions.cpp
+    echo. >> %Name%\Project\src\Functions.cpp
+    echo // Functions content >> %Name%\Project\src\Functions.cpp
 
     :: Message de succès
     echo Fichiers .cpp cree avec succes.
@@ -140,15 +148,15 @@ if /i "%type%"=="c" (
     echo Creation des fichiers .hpp ...
         
     :: Création des fichiers .hpp
-    echo. > Project_Name\Project\inc\Functions.hpp
+    echo. > %Name%\Project\inc\Functions.hpp
 
     :: Remplissage par défaut Functions.h
-    echo #ifndef __FUNCTIONS__ > Project_Name\Project\inc\Functions.hpp
-    echo #define __FUNCTIONS__ >> Project_Name\Project\inc\Functions.hpp
-    echo. >> Project_Name\Project\inc\Functions.hpp
-    echo // Header content >> Project_Name\Project\inc\Functions.hpp
-    echo. >> Project_Name\Project\inc\Functions.hpp
-    echo #endif >> Project_Name\Project\inc\Functions.hpp
+    echo #ifndef __FUNCTIONS__ > %Name%\Project\inc\Functions.hpp
+    echo #define __FUNCTIONS__ >> %Name%\Project\inc\Functions.hpp
+    echo. >> %Name%\Project\inc\Functions.hpp
+    echo // Header content >> %Name%\Project\inc\Functions.hpp
+    echo. >> %Name%\Project\inc\Functions.hpp
+    echo #endif >> %Name%\Project\inc\Functions.hpp
 
     echo Fichiers .hpp cree avec succes.
 )
